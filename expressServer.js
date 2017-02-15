8ab4c360fd0b68fbaa9c09cf204e1c0e50776a4c
fresh5447 <doug@kosmojo.com>
2017-02-15 12:13:48 -0700

    add new file expressServer which uses express as the server, and ejs as the templating engine

---
+++ expressServer.js
@@ -0,0 +1,30 @@
+var express = require('express'),
+    app = express(),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+    
+app.set('view engine', 'ejs');
+
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+
+    assert.equal(null, err);
+    console.log("Successfully connected to MongoDB.");
+
+    app.get('/', function(req, res){
+
+        db.collection('movies').find({}).toArray(function(err, docs) {
+            res.render('movies', { 'movies': docs } );
+        });
+
+    });
+
+    app.use(function(req, res){
+        res.sendStatus(404);
+    });
+
+    var server = app.listen(3000, function() {
+        var port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+    });
+
+});
